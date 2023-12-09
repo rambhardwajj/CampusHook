32b3e19db9fa4a60d53a642a46dbcf20b1c65a86
jeet1703 <prabhjeetdec03@gmail.com>
2023-12-09 20:02:30 +0530

    initial

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
+"use client"
+import React from 'react'
+import StudentLogin from './components/StudentLogin'
+import StudentSignUp from './components/StudentSignUp'
+import Navbar from './components/Navbar'
+
+
+const App = () => {
   return (
     <>
-       
+      <StudentSignUp/>
+      <StudentLogin/>
     </>
   )
 }
